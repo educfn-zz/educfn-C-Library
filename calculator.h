cbb34aff3a039fd9c9c62090bb0ea2e562982d89
Eduardo Cardoso Fernandes Neto <eduardo.cardoso10@gmail.com>
2018-02-06 21:56:43 -0200

    The first files for GitHub developing test.

---
+++ calculator.h
@@ -0,0 +1,11 @@
+//This is a simple project for mere practice of C language and for testing GitHub functionalities.
+//Author:Eduardo Cardoso Fernandes Neto - Computer Science Student
+
+#ifndef CALCULATOR_H_
+#define CALCULATOR_H_
+
+
+
+
+#endif // CALCULATOR_H_
+
